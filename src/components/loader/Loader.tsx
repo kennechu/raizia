e39121f0afeaff6cea035a7107ea281594cecf5c
Diego Monroy <Diego.Monroy@walmart.com>
2023-05-13 10:10:36 -0600

    modify loader

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,36 +1,19 @@
 import { Typography } from '@mui/material'
-import * as images from '../../utils/images'
-import { useEffect, useMemo, useState } from 'react'
+import { loader } from '../../utils/images'
 
 const Loader = () => {
-  const arrayImages = useMemo(() => [images.load1, images.load2, images.load3], [])
-  const [imgSrc, setImgSrc] = useState(arrayImages[0])
-  const renderImage = (src: string) => {
-    return (
-      <div className='flex flex-col justify-center items-center gap-10 object-cover'>
-        <img src={src} alt='logo' className='w-2/5 shadow-sm' />
-      </div>
-    )
-  }
-  const Mat = Math.floor(Math.random() * arrayImages.length)
-  useEffect(() => {
-    const interval = setInterval(() => {
-      return setImgSrc(arrayImages[Mat])
-    }, 900)
-    return () => clearInterval(interval)
-  }, [arrayImages, Mat])
-
-  setTimeout(() => {
-    window.location.href = '/policy'
-  }, 3000)
-
   return (
     <div className='flex flex-col justify-center items-center gap-10'>
       <div className='flex flex-col gap-4 justify-center items-center'>
         <Typography variant='h4' color='gray' fontWeight={600}>
           Buscando el mejor broker para ti
         </Typography>
-        {renderImage(imgSrc)}
+        <div className="flex space-x-2 animate-pulse">
+          <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
+          <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
+          <div className="w-3 h-3 bg-gray-500 rounded-full"></div>
+        </div>
+        <img src={loader} alt='loader' className='w-72 aspect-square object-contain' />
       </div>
     </div>
   )
